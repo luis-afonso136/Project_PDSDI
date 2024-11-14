f6dff2b96785ab7588d4c50cf6860744ff65d706
Luis Afonso <luis.manuel.afonso@ipvc.pt>
2024-11-14 10:10:52 +0000

    Update App.tsx

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
-// import Footer from '../client/components/footer';
-// import Home from '../client/screens/home';
-import Login from '../client/screens/login';
+import Footer from '../client/components/footer';
+import Home from '../client/screens/home';
+// import Login from '../client/screens/login';
 export const App = () => {
   return (
     <div className="App">
-      {/* <Home />
-      <Footer /> */}
-      <Login />
+      <Home />
+      <Footer />
+      {/* <Login /> */}
     </div>
   );
 };
